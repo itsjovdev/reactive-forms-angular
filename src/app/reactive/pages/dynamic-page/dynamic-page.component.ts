26604be4b9da832ae828f68819277d1c6f4b655c
Joan Vasquez <joanvrivas.21@gmail.com>
2025-05-15 02:03:11 +0200

    feat: dynamic forms are added with arrays

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,33 @@
 import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
 })
-export class DynamicPageComponent { }
+export class DynamicPageComponent {
+  private fb = inject(FormBuilder);
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array(
+      [
+        ['metal gear', Validators.required],
+        ['final fantasy', Validators.required],
+      ],
+      Validators.minLength(2) // At least one game is required
+    ),
+  });
+  // FormArray es un arreglo que contiene varios FormControls
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+}
