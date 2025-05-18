d788114eaddd811f07aff2e5706ba26276bb15e7
JoanAutopractik <joan@autopractik.es>
2025-05-18 20:16:57 +0200

    feat: add form utils to centralize and reuse validation logic

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,22 +1,11 @@
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 //Este archivo contiene funciones utilitarias  y reutilizablees para tranajar con formularios reactivos
 //es de responsabilidad unica porque su una funcion es de proporcionar una funciona de ayuda para validar campos de formulario
 //reutilizacion porque se puede utilizar en muchos formularios
 export class FormUtils {
-  //estaticos porque se usan sin instanciar la clase, por ejemplo FormUtils.isValidField()
-  static isValidField(form: FormGroup, fieldName: string): boolean | null {
-    return (
-      !!form.controls[fieldName].errors && form.controls[fieldName].touched
-    );
-  }
-
-  static getFieldError(form: FormGroup, fieldName: string): string | null {
-    //1.- verificamos si el campo existe enn el formulario, si no existe retornamos null
-    if (!form.controls[fieldName]) return null;
-    //2.- la variable errors contiene los errores del campo, si no existe errores, asignamos un objeto vacio para evitar errores  al iterar
-    const errors = form.controls[fieldName].errors ?? {};
-    //3.- recorremos los tipos de errores del objeto 'errors'
-    for (const key of Object.keys(errors)) {
+  
+  static getTextError(errors: ValidationErrors){
+      for (const key of Object.keys(errors)) {
       //4.- dependiendo del tipo de error, retornamos un mensaje diferente
       switch (key) {
         case 'required':
@@ -27,7 +16,38 @@ export class FormUtils {
           return `Valor minimo de ${errors['min'].min}`;
       }
     }
-    //5.- si no se encuentra un error conocido, retornamos null
     return null;
   }
-}
+  
+  //estaticos porque se usan sin instanciar la clase, por ejemplo FormUtils.isValidField()
+  static isValidField(form: FormGroup, fieldName: string): boolean | null {
+    return (
+      !!form.controls[fieldName].errors && form.controls[fieldName].touched
+    );
+  }
+
+  static getFieldError(form: FormGroup, fieldName: string): string | null {
+    //1.- verificamos si el campo existe enn el formulario, si no existe retornamos null
+    if (!form.controls[fieldName]) return null;
+    
+   const errors = form.controls[fieldName].errors ?? {};
+
+   return FormUtils.getTextError(errors)
+
+  }
+
+ static isValidFieldInArray(formArray: FormArray, index: number){
+    return(
+      formArray.controls[index].errors && formArray.controls[index].touched
+    )
+  }
+
+    static getFieldErrorInArray(formArray: FormArray, index: number): string |null {
+    if (formArray.controls.length == 0) return null;
+    const errors = formArray.controls[index].errors  ?? {};
+
+    return FormUtils.getTextError(errors)
+
+    }
+  }
+
